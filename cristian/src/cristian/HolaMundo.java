5ce16bd28c030db585d04a92cd90d242affd321a
Cristian <cpachecoarjona@gmail.com>
2022-03-30 09:52:55 +0200

    primer commit

--- cristian/src/cristian/HolaMundo.java
+++ cristian/src/cristian/HolaMundo.java
@@ -5,6 +5,7 @@ public class HolaMundo {
 	public static void main(String[] args) {
 		System.out.println("Hola mundo");
 		System.out.println("Modificacion");
+		System.out.println("Cambio 1");
 	}
 
 }
